87b082aedd98a0aba97924d17d6c9be51e521c32
Kyaw Min Hein <kyawminhein101@gmail.com>
2023-05-18 09:52:18 +0630

    composer_autoload

--- edit.php
+++ edit.php
@@ -1,6 +1,6 @@
 <?php
 
-    require_once "db.php";
+require_once "vendor/autoload.php";
 
     $db = new DB();
 
---
+++ helpers.php
@@ -0,0 +1,7 @@
+<?php
+
+    function dd($var) {
+        die(var_dump($var));
+    }
+
+?>
